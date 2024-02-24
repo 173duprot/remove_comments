c207d174c070c1787a71ff9460c0e462d9603e81
173duprot <173duprot@protonmail.com>
2024-02-24 18:43:41 -0500

    almost done

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,23 @@
+extern crate regex;
+
 use regex::Regex;
 
-// Define a tuple to hold language-specific comment syntax
-type Language = (&'static str, &'static str, &'static str, &'static str);
-
-// Function to remove comments from code based on language syntax
-fn remove_comments(code: &str, lang: &Language) -> String {
-    let single_re = Regex::new(&format!(
-        r"{}[^\n\r]*",
-        regex::escape(lang.1)
-    ))
-    .unwrap();
-    let multi_re = Regex::new(&format!(
-        r"(?s){}.*?{}",
-        regex::escape(lang.2),
-        regex::escape(lang.3)
-    ))
-    .unwrap();
-
-    let code = single_re.replace_all(&code, "");
-    let code = multi_re.replace_all(&code, "");
-
-    code.into_owned()
-}
-
 fn main() {
-    // Define supported languages
-    let langs: Vec<Language> = vec![
-        ("C", "//", "/*", "*/"),
-        ("C++", "//", "/*", "*/"),
-        ("Java", "//", "/*", "*/"),
-        ("C#", "//", "/*", "*/"),
-    ];
-
-    // Example usage
-    let code = r#"
-        // Single line comment
-        /* Multi
-           line
-           comment */
-        int main() {
-            // Hello, world!
-            return 0;
-        }
+    let input = r#"
+    This is a block of text. As John Smith says /*to be or 
+    not to be*/ but what is the question? // This is a comment
     "#;
 
-    // Remove comments for each language
-    for lang in &langs {
-        println!("{} code without comments:\n{}", lang.0, remove_comments(code, lang));
-    }
+    let output = Regex::new(r#"(?s)/\*.*?\*/|//.*$"#).unwrap()
+        .replace_all(&input, |caps: &regex::Captures| {
+            caps.get(0).unwrap().as_str()
+                .chars()
+                .map(|c| if c == '\n' || c == '\r' { c } else { ' ' })
+                .collect::<String>()
+        });
+
+    assert_eq!(input.lines().count(), output.lines().count(), "Number of lines changed!");
+
+    println!("{}", output);
 }
 
