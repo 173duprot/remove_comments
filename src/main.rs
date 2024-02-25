756b9219e63943d06bb3a99bcd1aa00fdf564f98
173duprot <173duprot@protonmail.com>
2024-02-25 12:14:20 -0500

    Removed main.rs

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-use regex::Regex;
-use std::fs;
-
-fn main() {
-    let input = fs::read_to_string("input.txt").unwrap();
-    let output = Regex::new(r#"// .*|(?s)/\*.*?\*/"#).unwrap().replace_all(&input, "");
-
-    println!("{}", output);
-}
