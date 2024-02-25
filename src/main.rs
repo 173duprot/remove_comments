a324ab120c3dd5c88513eacbb7c1b7eab2497bf4
173duprot <173duprot@protonmail.com>
2024-02-24 19:53:01 -0500

    Figured out the bug: the single-line code needs to go before the multi-line because the multi-line code changes modes.

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,12 @@ use std::fs;
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Failed to read input file");
 
-    let comment_regex = r#"//.*(?:\n|\r\n?)|(?s)/\*.*?\*/"#;
-    let output = Regex::new(comment_regex).unwrap()
+    let output = Regex::new(r#"// .*|(?s)/\*.*?\*/"#).unwrap()
         .replace_all(&input, |caps: &regex::Captures| {
-            if let Some(comment) = caps.get(0) {
-                let matched_text = comment.as_str();
-                matched_text
-                    .chars()
-                    .map(|c| if c == '\n' || c == '\r' { c } else { ' ' })
-                    .collect::<String>()
-            } else {
-                String::new()
-            }
+            caps.get(0).unwrap().as_str()
+                .chars()
+                .map(|c| if c == '\n' || c == '\r' { c } else { ' ' })
+                .collect::<String>()
         });
 
     assert_eq!(input.lines().count(), output.lines().count(), "Number of lines changed!");
